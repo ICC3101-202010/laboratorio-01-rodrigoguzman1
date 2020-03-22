a1ed8051cfc9b8b509a26d05d9ec7c93d399258e
Rodrigo Guzman <rodrigoguzman8@gmail.com>
2020-03-22 20:05:33 -0300

    first commit

---
+++ Laboratorio1/Laboratorio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Laboratorio1
+{
+
+    class Persona
+    {
+        string Nombre;
+        string Apellido;
+
+        public int Lanzar()
+        {
+            int randomInt = new Random().Next(0, 3);
+            return randomInt;
+        }
+        
+    }
+
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            Console.WriteLine(persona.Lanzar());
+        }
+    }
+}
